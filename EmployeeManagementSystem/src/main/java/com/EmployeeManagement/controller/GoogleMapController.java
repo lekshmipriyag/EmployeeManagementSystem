8b8a2925b28d3cd222204364d4711e15b5e83f57
LekshmyPriya <s3767475@student.rmit.edu.au>
2022-03-28 16:32:32 +1100

    Google Map API

--- EmployeeManagementSystem/src/main/java/com/EmployeeManagement/controller/GoogleMapController.java
+++ EmployeeManagementSystem/src/main/java/com/EmployeeManagement/controller/GoogleMapController.java
@@ -1,18 +1,21 @@
 package com.EmployeeManagement.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/employee")
 public class GoogleMapController {
 
+    @Value("${server.url}")
+    private String employeeURL;
+
     @GetMapping("/findLocation")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greeting(Model model) {
+        model.addAttribute("employeeURL", employeeURL);
         return "searchLocation";
     }
 
